fcb145933ea17a1aab782ea3f0c98b9457116ccb
Leonid Lisovskij <leonid_lisovskij@epam.com>
2025-11-11 18:00:10 +0200

    create pytest checks and reports

--- PyTest Introduction/tests/conftest.py
+++ PyTest Introduction/tests/conftest.py
@@ -2,9 +2,36 @@ import pytest
 import pandas as pd
 
 # Fixture to read the CSV file
-
-
+@pytest.fixture(scope="session")
+def df(path_to_file='../src/data/data.csv'):
+    """
+    Loads a CSV file into a pandas DataFrame.
+    """
+    return pd.read_csv(path_to_file)
+    
+ 
 # Fixture to validate the schema of the file
+@pytest.fixture(scope="session")
+def expected_schema():
+    return {
+        'id': 'int64',
+        'name': 'object',
+        'age': 'int64',
+        'email': 'object',
+        'is_active': 'bool',
+    }
 
+@pytest.fixture(scope="session")
+def actual_schema(df):
+    return {col: str(df[col].dtype) for col in df.columns}
+
+@pytest.fixture(scope="session")
+def schemas(actual_schema, expected_schema):
+    return actual_schema,expected_schema
 
 # Pytest hook to mark unmarked tests with a custom mark
+def pytest_collection_modifyitems(config, items):
+   for item in items:
+       if not getattr(item, "own_markers", []):
+        item.add_marker(pytest.mark.unmarked)
+
--- PyTest Introduction/tests/test_csv/test_csv_file.py
+++ PyTest Introduction/tests/test_csv/test_csv_file.py
@@ -2,29 +2,48 @@ import pytest
 import re
 
 
-def test_file_not_empty():
-    assert 1 + 1 == 2
+def test_file_not_empty(df):
+    assert df.shape[0] > 0, "Data Entity is empty!" 
 
 
-def test_duplicates():
-    assert 1 + 1 == 2
+@pytest.mark.validate_csv
+@pytest.mark.xfail
+def test_duplicates(df):
+    assert df.duplicated().sum() == 0, "Duplicates are found!" 
 
 
-def test_validate_schema():
-    assert 1 + 1 == 2
+@pytest.mark.validate_csv
+def test_validate_schema_attributes(schemas):
+    actual_schema, expected_schema = schemas
+    assert set(actual_schema.keys()) == set(expected_schema.keys()), "Attribute names do not match!"
 
 
-def test_age_column_valid():
-    assert 1 + 1 == 2
+@pytest.mark.validate_csv
+def test_validate_schema_datatypes(schemas):
+    actual_schema, expected_schema = schemas
+    for item in expected_schema:
+        assert actual_schema[item] == expected_schema[item], "Data Types do not match!"
 
 
-def test_email_column_valid():
-    assert 1 + 1 == 2
+@pytest.mark.validate_csv
+@pytest.mark.skip
+def test_age_column_valid(df):
+    assert ((df['age'] >= 0) & (df['age'] <= 100)).all(), "Age is invalid!" 
+
+@pytest.mark.validate_csv
+def test_email_column_valid(df):
+    email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z]+$"
+    assert df['email'].str.match(email_pattern).all(), "Email is invalid!" 
 
 
-def test_active_players():
-    assert 1 + 1 == 2
+@pytest.mark.parametrize("id, is_active",[(1,False),(2, True)])
+def test_active_players(df,id,is_active):
+    df_for_assertion = df[df['id'] == id]
+    assert df_for_assertion.shape[0] > 0,"Predefined ID is missing!" 
+    assert (df_for_assertion['is_active'] == is_active).all(),"ID active status is not as expected!" 
 
 
-def test_active_player():
-    assert 1 + 1 == 2
+def test_active_player_two(df):
+    df_for_player_two = df[df['id'] == 2]
+    assert df_for_player_two.shape[0] > 0,"Predefined ID is missing!" 
+    assert (df_for_player_two['is_active'] == True).all(),"Player two status is not Active!"
